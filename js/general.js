ed74e53de7de754ac5bb9a8c98539d1b42e6741c
MitchP404 <75387517+MitchP404@users.noreply.github.com>
2024-11-18 22:02:12 -0500

    Documented css and js code

--- js/general.js
+++ js/general.js
@@ -1,16 +1,26 @@
 
+/*
+
+	This page contains generic JavaScript functions and variables used on mutiple pages
+
+*/
+
+// Display the cookie settings bar
 function showCookieSettings(){
 	document.getElementById("cookieBar").style.display = "flex";
 }
 
+// Close the cookie settings bar
 function closeBar() {
 	document.getElementById("cookieBar").style.display = "none";
 }
 
+// Enable cookies. As the cookies in this page will not be implemented in this class, this function simply closes the bar
 function enableCookies() {
 	closeBar();
 }
 
+// Disable cookies. As the cookies in this page will not be implemented in this class, this function simply closes the bar
 function disableCookies() {
 	closeBar();
 }
